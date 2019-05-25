2bfffcc49f642a39da1fcb018a1afefba47c95e7
Hasan <samiulcse@yahoo.com>
2019-05-25 12:38:39 +0600

    lesson-5-Getting-user-form-data done

--- src/view/Signup.vue
+++ src/view/Signup.vue
@@ -6,19 +6,19 @@
           <h3 class="text-center mb-5">Sign Up</h3>
 
           <div class="form-group">
-            <input type="text" placeholder="Name" class="form-control">
+            <input v-model="name" type="text" placeholder="Name" class="form-control">
           </div>
 
           <div class="form-group">
-            <input type="text" placeholder="Email" class="form-control">
+            <input v-model="email" type="text" placeholder="Email" class="form-control">
           </div>
 
           <div class="form-group">
-            <input type="password" placeholder="Password" class="form-control">
+            <input v-model="password" type="password" placeholder="Password" class="form-control">
           </div>
 
           <div class="form-group">
-            <button class="btn btn-success text-center form-control">Sign Up</button>
+            <button @click="registerUser()" class="btn btn-success text-center form-control">Sign Up</button>
           </div>
 
         </div>
@@ -26,3 +26,23 @@
     </div>
   </div>
 </template>
+
+
+<script>
+export default {
+    data(){
+        return {
+            name:'',
+            email:'',
+            password:''
+        }
+    },
+
+
+    methods: {
+        registerUser(){
+            
+        }
+    },
+}
+</script>
